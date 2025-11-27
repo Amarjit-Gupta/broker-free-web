8a71f894d6221c29c8b9a403cda9f68d976f1ceb
Amarjit-Gupta <168736859+Amarjit-Gupta@users.noreply.github.com>
2025-11-27 12:52:04 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin:["http://localhost:5173","https://broker-free-web-94hi.vercel.app/"]
+    origin:["http://localhost:5173","https://broker-free-web-hezh.vercel.app"]
 }));
 
 app.get("/",(req,res)=>{
